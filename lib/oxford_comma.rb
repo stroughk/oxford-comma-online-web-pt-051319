3104a0ce151b1a0d39f47d2413f630389abd243e
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-20 00:34:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+array = ["fiddleheads","okra","kohlrabi"]
 
+def oxford_comma(array)
+  if array.length == 2 
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end 
+  array.join(", ")
 end
+
+puts oxford_comma(array) 
