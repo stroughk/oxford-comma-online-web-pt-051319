35c1721a5f43223897c134f7f9f181e538f94c10
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-23 02:18:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,12 @@
 fruits = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
 
 def oxford_comma(fruits)
-  fruits.each do |fruit|
-  
-  if fruits.length == 1 
-    fruits == fruits
-  elsif fruits.length == 2 
-    puts |fruit| + "and" + "fruit"
-  end 
-    puts |fruit| 
+  if fruits.length == 2
+    return "#{fruits[0]} and #{fruits[1]}"
+  else fruits.length > 2
+    fruits[-1].insert(0, "and ")
   end
+    fruits.join(", ")
 end
+
+puts oxford_comma(fruits)
