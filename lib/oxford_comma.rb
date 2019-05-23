fac76e5c12919616a8264250f0127a8a26c99167
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-23 02:19:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,10 @@ fruits = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "
 def oxford_comma(fruits)
   if fruits.length == 2
     return "#{fruits[0]} and #{fruits[1]}"
-  else fruits.length > 2
+  elsif fruits.length > 2
     fruits[-1].insert(0, "and ")
+  else fruits.length == 1
+    return fruits
   end
     fruits.join(", ")
 end
