e6da4d7a04e76f131202d735c63f3e4bdb22665a
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-23 02:21:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,14 @@
-array = ["fiddleheads","okra","kohlrabi"]
+fruits = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
 
-def oxford_comma(array)
-  if array.length == 2 
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end 
-  array.join(", ")
+def oxford_comma(fruits)
+  if fruits.length == 2
+    return "#{fruits[0]} and #{fruits[1]}"
+  elsif fruits.length > 2
+    fruits[-1].insert(0, "and ")
+  else fruits.length == 1
+    fruits
+  end
+    fruits.join(", ")
 end
 
-puts oxford_comma(array) 
+puts oxford_comma(fruits)
